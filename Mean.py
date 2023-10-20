cd7a154e5754e4787d44f43cbfde6c2fc971ce07
maxi910 <maximilianoprida@gmail.com>
2023-10-20 10:53:47 -0300

    hola

--- Mean.py
+++ Mean.py
@@ -1,19 +1,21 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import gmean, hmean
+from datos import APITiingo, APIStub  # Comentado porque no tengo el módulo datos
+import matplotlib.pyplot as plt
 
 class CalculadoraDePromedios:
     def __init__(self, datos):
-        self.datos = datos  # Asumiendo que 'datos' es un DataFrame de pandas
+        self.datos = datos
 
     def media_aritmetica(self, columna):
         return self.datos[columna].mean()
 
     def media_geometrica(self, columna):
-        return gmean(self.datos[columna].dropna())  # Eliminando NaN antes de calcular la media geométrica
+        return gmean(self.datos[columna].dropna())
 
     def media_armonica(self, columna):
-        return hmean(self.datos[columna].dropna())  # Eliminando NaN antes de calcular la media armónica
+        return hmean(self.datos[columna].dropna())
 
     def media_movil(self, columna, ventana):
         return self.datos[columna].rolling(window=ventana).mean()
@@ -21,12 +23,27 @@ class CalculadoraDePromedios:
     def media_movil_exponencial(self, columna, span):
         return self.datos[columna].ewm(span=span).mean()
 
-# Ejemplo de uso
-# Asegúrate de reemplazar 'df' con el DataFrame que obtienes de la clase en datos.py
+tickers_usuario = input("Ticker: ") 
+tickers_usuario = [ticker.strip() for ticker in tickers_usuario] 
+
+df = pd.DataFrame({ticker: np.random.normal(size=100) for ticker in tickers_usuario}) 
+
 calculadora = CalculadoraDePromedios(df)  
 
-# Calculando la media móvil exponencial para una columna específica 
-# (reemplaza 'columna' con el nombre real de tu columna y 'span' con el valor que desees)
-media_movil_exponencial = calculadora.media_movil_exponencial('columna', span=20)
+# Solicitar al usuario que ingrese el nombre de la columna
+columna = input("Ingrese el nombre de la columna para calcular la media móvil exponencial: ").strip()
 
-print("Media Móvil Exponencial (span de 20):", media_movil_exponencial)
+if columna in df.columns:
+    media_movil_exponencial = calculadora.media_movil_exponencial(columna, span=20)
+    print(f"Media Móvil Exponencial (span de 20) para {columna}:", media_movil_exponencial)
+
+    plt.figure(figsize=(10,6))
+    plt.plot(media_movil_exponencial, label=f'Media Móvil Exponencial (span de 20) para {columna}')
+    plt.title(f'Media Móvil Exponencial para {columna}')
+    plt.xlabel('Índice')
+    plt.ylabel('Valor')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+else:
+    print(f"La columna '{columna}' no se encuentra en el DataFrame.")
